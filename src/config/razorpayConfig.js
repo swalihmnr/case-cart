e4b9c7b8caef76bbd9a37bb724264c5ce8b2595e
swalimnr <swalimohd048@gmail.com>
2026-02-17 15:32:43 +0530

    feat(online payment):create the insteance of razorpay

---
+++ src/config/razorpayConfig.js
@@ -0,0 +1,10 @@
+import Razorpay from "razorpay";
+import env from 'dotenv';
+env.config()
+
+const razorpayInstence=new Razorpay({
+    key_id:process.env.RAZORPAY_KEY_ID,
+    key_secret:process.env.RAZORPAY_SECRET
+})
+
+export default razorpayInstence
