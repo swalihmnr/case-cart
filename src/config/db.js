32aa00eb2818c4d31b7384efea575f4d4e1a0882
swalimnr <swalimohd048@gmail.com>
2026-03-12 22:47:32 +0530

    fixed some bugs by integerating db atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log(`db connected on ${process.env.MONGO_URI}`);
+
+    console.log(" MongoDB connected successfully");
   } catch (err) {
-    console.log(`db is not connectd to ${process.env.MONGO_URI}`);
+    console.error(" MongoDB connection failed:", err.message);
+
+    process.exit(1);
   }
 };
+
 export default connectDB;
